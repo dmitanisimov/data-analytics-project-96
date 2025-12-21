b3777d92cd48d0026ac78fbf045819ced8969c68
dmitanisimov <30756941+dmitanisimov@users.noreply.github.com>
2025-12-21 21:06:11 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -2,10 +2,6 @@ WITH joined AS (
     SELECT
         t.visitor_id,
         t.visit_date::date AS visit_date,
-        CASE
-            WHEN t.source ILIKE 'vk%' THEN 'vk'
-            ELSE t.source
-        END AS utm_source,
         t.medium AS utm_medium,
         t.campaign AS utm_campaign,
         l.lead_id,
@@ -13,18 +9,23 @@ WITH joined AS (
         l.amount,
         l.closing_reason,
         l.status_id,
+        CASE
+            WHEN t.source ILIKE 'vk%' THEN 'vk'
+            ELSE t.source
+        END AS utm_source,
         ROW_NUMBER() OVER (
             PARTITION BY t.visitor_id
             ORDER BY t.visit_date DESC
         ) AS rnk
-    FROM sessions t
-    LEFT JOIN leads l
-      ON t.visitor_id = l.visitor_id
-     AND t.visit_date <= l.created_at
+    FROM sessions AS t
+    LEFT JOIN leads AS l
+        ON
+            t.visitor_id = l.visitor_id
+            AND t.visit_date <= l.created_at
     WHERE t.medium <> 'organic'
 ),
 
-SesLead AS (
+seslead AS (
     SELECT *
     FROM joined
     WHERE rnk = 1
@@ -42,14 +43,14 @@ grouped AS (
 
         COUNT(lead_id) FILTER (
             WHERE closing_reason = 'Успешно реализовано'
-               OR status_id = 142
+            OR status_id = 142
         ) AS purchases_count,
 
         SUM(amount) FILTER (
             WHERE closing_reason = 'Успешно реализовано'
-               OR status_id = 142
+            OR status_id = 142
         ) AS revenue
-    FROM SesLead
+    FROM seslead
     GROUP BY
         visit_date,
         utm_source,
@@ -65,10 +66,22 @@ costs AS (
         utm_campaign,
         SUM(daily_spent) AS total_cost
     FROM (
-        SELECT campaign_date, utm_source, utm_medium, utm_campaign, daily_spent FROM ya_ads
+        SELECT
+            campaign_date,
+            utm_source,
+            utm_medium,
+            utm_campaign,
+            daily_spent
+        FROM ya_ads
         UNION ALL
-        SELECT campaign_date, utm_source, utm_medium, utm_campaign, daily_spent FROM vk_ads
-    ) ads
+        SELECT
+            campaign_date,
+            utm_source,
+            utm_medium,
+            utm_campaign,
+            daily_spent
+        FROM vk_ads
+    ) AS ads
     GROUP BY
         campaign_date::date,
         utm_source,
@@ -87,12 +100,13 @@ final_data AS (
         g.purchases_count,
         g.revenue,
         c.total_cost
-    FROM grouped g
-    LEFT JOIN costs c
-      ON g.visit_date = c.visit_date
-     AND g.utm_source = c.utm_source
-     AND g.utm_medium = c.utm_medium
-     AND g.utm_campaign = c.utm_campaign
+    FROM grouped AS g
+    LEFT JOIN costs AS c
+        ON
+            g.visit_date = c.visit_date
+            AND g.utm_source = c.utm_source
+            AND g.utm_medium = c.utm_medium
+            AND g.utm_campaign = c.utm_campaign
 )
 
 SELECT
@@ -104,10 +118,10 @@ SELECT
     SUM(revenue) AS revenue,
     SUM(total_cost) AS total_cost,
 
-	ROUND(SUM(total_cost) / SUM(visitors_count)) AS cpu,
-	ROUND(SUM(total_cost) / SUM(leads_count)) AS cpl,
-	ROUND(SUM(total_cost) / SUM(purchases_count)) AS cppu,
-	ROUND((SUM(revenue) - SUM(total_cost)) / SUM(total_cost) * 100) AS roi
+    ROUND(SUM(total_cost) / SUM(visitors_count)) AS cpu,
+    ROUND(SUM(total_cost) / SUM(leads_count)) AS cpl,
+    ROUND(SUM(total_cost) / SUM(purchases_count)) AS cppu,
+    ROUND((SUM(revenue) - SUM(total_cost)) / SUM(total_cost) * 100) AS roi
 
 FROM final_data
 WHERE utm_source IN ('vk', 'yandex')
