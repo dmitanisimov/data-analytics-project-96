0aa0512e1c98e36dc8f64017e64ac78eb64185dd
dmitanisimov <30756941+dmitanisimov@users.noreply.github.com>
2025-12-21 21:10:37 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,47 +1,48 @@
-
-
-
 with tab as (
-select * from sessions 
-where medium IN ('cpc', 'cpm', 'cpa','youtube','cpp','tg','social')
+    select * from sessions
+    where medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
 ),
-joined AS (
-  SELECT 
-    t.visitor_id,
-    t.visit_date,
-    t.source AS utm_source,
-    t.medium AS utm_medium,
-    t.campaign AS utm_campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id
-  FROM tab as t
-  LEFT JOIN leads l ON t.visitor_id = l.visitor_id
-  WHERE l.created_at IS NULL OR t.visit_date <= l.created_at
+
+joined as (
+    select
+        t.visitor_id,
+        t.visit_date,
+        t.source as utm_source,
+        t.medium as utm_medium,
+        t.campaign as utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id
+    from tab as t
+    left join leads as l on t.visitor_id = l.visitor_id
+    where l.created_at is NULL or t.visit_date <= l.created_at
 ),
-rank AS (
-  SELECT *,
-         ROW_NUMBER() OVER (PARTITION BY lead_id ORDER BY visit_date DESC) AS rnk
-  FROM joined
+
+rank as (
+    select
+        *,
+        ROW_NUMBER() over (partition by lead_id order by visit_date desc) as rnk
+    from joined
 )
-SELECT
-  visitor_id,
-  visit_date,
-  utm_source,
-  utm_medium,
-  utm_campaign,
-  lead_id,
-  created_at,
-  amount,         
-  closing_reason,
-  status_id
-FROM rank
-WHERE rnk = 1
-ORDER BY
-  amount DESC NULLS LAST,
-  visit_date ASC,
-  utm_source,
-  utm_medium,
-  utm_campaign;
+
+select
+    visitor_id,
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
+from rank
+where rnk = 1
+order by
+    amount desc nulls last,
+    visit_date asc,
+    utm_source asc,
+    utm_medium asc,
+    utm_campaign asc;
