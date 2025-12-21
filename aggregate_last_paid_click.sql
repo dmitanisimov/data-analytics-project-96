29a0f8c65173e4b55011d80c1a8093568b96c527
dmitanisimov <30756941+dmitanisimov@users.noreply.github.com>
2025-12-21 20:47:16 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,42 +1,32 @@
 with joined as (
     select
         t.visitor_id,
+        t.visit_date::date as visit_date,
         t.medium as utm_medium,
         t.campaign as utm_campaign,
-        t.visit_date::date as visit_date,
-        case
-            when t.source ilike 'vk%' then 'vk'
-            else t.source
-        end as utm_source,
         l.lead_id,
         l.created_at,
         l.amount,
         l.closing_reason,
         l.status_id,
+        case
+            when t.source ilike 'vk%' then 'vk'
+            else t.source
+        end as utm_source,
         row_number() over (
             partition by t.visitor_id
             order by t.visit_date desc
         ) as rnk
     from sessions as t
     left join leads as l
-        on t.visitor_id = l.visitor_id
-        and t.visit_date <= l.created_at
+        on
+            t.visitor_id = l.visitor_id
+            and t.visit_date <= l.created_at
     where t.medium <> 'organic'
 ),
 
 seslead as (
-    select
-        visitor_id,
-        utm_medium,
-        utm_campaign,
-        visit_date,
-        utm_source,
-        lead_id,
-        created_at,
-        amount,
-        closing_reason,
-        status_id,
-        rnk
+    select *
     from joined
     where rnk = 1
 ),
@@ -51,11 +41,11 @@ grouped as (
         count(distinct lead_id) as leads_count,
         count(lead_id) filter (
             where closing_reason = 'Успешно реализовано'
-                or status_id = 142
+            or status_id = 142
         ) as purchases_count,
         sum(amount) filter (
             where closing_reason = 'Успешно реализовано'
-                or status_id = 142
+            or status_id = 142
         ) as revenue
     from seslead
     group by
@@ -108,10 +98,11 @@ select
     g.revenue
 from grouped as g
 left join costs as c
-    on g.visit_date = c.visit_date
-    and g.utm_source = c.utm_source
-    and g.utm_medium = c.utm_medium
-    and g.utm_campaign = c.utm_campaign
+    on
+        g.visit_date = c.visit_date
+        and g.utm_source = c.utm_source
+        and g.utm_medium = c.utm_medium
+        and g.utm_campaign = c.utm_campaign
 order by
     g.revenue desc nulls last,
     g.visit_date asc,
@@ -119,3 +110,4 @@ order by
     g.utm_source asc,
     g.utm_medium asc,
     g.utm_campaign asc;
+
